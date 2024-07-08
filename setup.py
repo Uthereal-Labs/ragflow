225e11b29b6a42af53358f32fa12a82feb48dc7a
Victor Jimenez <victor.jimenez.rodriguez@estudiantat.upc.edu>
2024-07-08 14:29:08 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        return file.read().splitlines()
+
+setup(
+    name='ragflow',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    author='Original Author',
+    author_email='author@example.com',
+    description='RAGFlow is an open-source RAG (Retrieval-Augmented Generation) engine.',
+    url='https://github.com/infiniflow/ragflow',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
